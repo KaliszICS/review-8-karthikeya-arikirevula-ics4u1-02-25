167c5cfda95b84fc435947a5cd033e868f4bab80
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-19 15:30:25 +0000

    ez

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static String pluralize(String word) {
+		if (word.toLowerCase().endsWith("ey")) {
+			return "eys";
+		} else if (word.toLowerCase().endsWith("y")) {
+			return "ies";
+		} else if (word.toLowerCase().endsWith("ife")) {
+			return "ives";
+		} else {
+			return "s";
+		}
+	}
+
+	public static int min(int num1, int num2, int num3) {
+		if (num1 < num2 && num1 < num3) {
+			return num1;
+		} else if (num2 < num1 && num2 < num3) {
+			return num2;
+		} else {
+			return num3;
+		}
+	}
+
+	public static boolean isLeapYear(int number) {
+		if (number % 4 == 0) {
+			if (number % 100 == 0) {
+				if (number % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
 }
